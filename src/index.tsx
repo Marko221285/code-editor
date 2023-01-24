c524e9d26fe8921235b4000cf0bad3d0ff32f8b2
Marko Horvat <horvat.marko2212@gmail.com>
2023-01-24 23:45:30 +0100

    transpile option

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,45 @@
-import React from "react";
+import * as esbuild from "esbuild-wasm";
+import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(
