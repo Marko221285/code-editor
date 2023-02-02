97e5ec6b775af7022c13f44eb2b7783aa23e0722
Marko Horvat <horvat.marko2212@gmail.com>
2023-02-02 16:13:24 +0100

    printing errors

--- src/index.tsx
+++ src/index.tsx
@@ -26,6 +26,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -47,7 +49,13 @@ const App = () => {
         <div id="root"></div>
         <script>
           window.addEventListener('message', (event) => {
-            eval(event.data);
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              throw err;
+            }
           }, false);
         </script>
       </body>
