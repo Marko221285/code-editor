6f03bf4f402218a48ce3926af64b6bd71ae613fb
Marko Horvat <horvat.marko2212@gmail.com>
2023-02-06 19:58:03 +0100

    handling async errors

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -11,19 +11,26 @@ const html = `
       <body>
         <div id="root"></div>
         <script>
+          const handleError = (err) => {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            throw err;
+          };
+          window.addEventListener('error', (event) => {
+            event.preventDefault();
+            handleError(event.error);
+          });
           window.addEventListener('message', (event) => {
             try {
               eval(event.data);
             } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              throw err;
+              handleError(err);
             }
           }, false);
         </script>
       </body>
     </html>
-  `;
+`;
 
 const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
